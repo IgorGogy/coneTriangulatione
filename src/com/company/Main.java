0d15a371a14881c827524a7e69770e5e5f6766cf
Igor <gogy1507@gmail.com>
2021-07-27 01:06:02 +0400

    small change countX();

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -39,14 +39,20 @@ public class Main {
 
     }
 
-    public static void countX(int radius, int height, int triangle) {
-    //double degrees1 = 0.0;
-    //double radians1 = Math.toRadians(degrees1);
-    //    System.out.format("Синус %.1f градусов равен %.4f%n", radians1, Math.sin(radians1));
+    public static double countX(int radius, int height, int triangle) {
+        double x;
+//        for (int i=0; i<triangle-1; i++) {
+//        x=radius*Math.cos(2*3.14*i/triangle);
+//            System.out.println("координата Х: " + x);
+        //double degrees1 = 0.0;
+        //double radians1 = Math.toRadians(degrees1);
+        //    System.out.format("Синус %.1f градусов равен %.4f%n", radians1, Math.sin(radians1));
         int t;
         t=2;
         System.out.format("Синус просто %.4f%n", Math.sin(1.6));
 
-};
+        x = 0;
+        return x;
+    };
 }
 
