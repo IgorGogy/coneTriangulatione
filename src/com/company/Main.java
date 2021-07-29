38765e515100624d6d920cf46aab60d439a3169f
Igor <gogy1507@gmail.com>
2021-07-29 15:28:29 +0400

    add comment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class Main {
@@ -15,15 +17,16 @@ public class Main {
         int triangle = in.nextInt();
         System.out.printf("Радиус: %s  высота: %s сегментов: %s  \n", radius, height, triangle);
         in.close();
-        outputPoints((inputX(triangle, radius)), (inputY(triangle, radius)), height);
+        outputPoints((inputX(triangle, radius)), (inputY(triangle, radius)), inputZ(triangle, height));
     }
 
-    private static void outputPoints(double[] inputX, double[] inputY, int height) {
+    private static void outputPoints(double[] inputX, double[] inputY, int[] inputZ) {
         System.out.println("Координаты точек:");
         for (int i = 0; i < inputX.length; i++) {
 System.out.println(String.format(
- "P%d: (" + "x=%.2f;  " + "y=%.2f;  " + "z=%d) ", i, inputX[i], inputY[i], height));
-                 }
+ "P%d: (" + "x=%.2f;  " + "y=%.2f;  " + "z=%d) ", i, inputX[i], inputY[3], inputZ[i])); }
+        System.out.println("Координаты точек треугольников:");
+//System.out.println(String.format("Треугольник%d: (" + "x=%.2f;  " + "y=%.2f;  " + "z=%d) ", i,
     }
 
     public static double[] inputX(int triangle, int radius) {
@@ -41,17 +44,30 @@ System.out.println(String.format(
         }
         return arrayY;
     }
+    public static int[] inputZ(int triangle, int height) {
+        int[] arrayZ = new int[triangle];
+        for (int k = 0; k < triangle; k++) {
+            arrayZ[k] = countZ(triangle, height, k);
+        }
+        return arrayZ;
+    }
 
     public static double countX(int triangle, int radius, int i) {
         double x = 0;
-        x = radius * Math.cos(2 * 3.14159265359 * i / triangle);
+        x = radius * Math.cos(6.28318531 * i / triangle);
         return x;
     }
 
     public static double countY(int triangle, int radius, int i) {
         double y = 0;
-        y = radius * Math.sin(2 * 3.14159265359 * i / triangle);
+        y = radius * Math.sin(6.28318531 * i / triangle);
         return y;
+    }
+
+    public static int countZ(int triangle, int height, int i) {
+        int z = 0;
+        z = height;
+        return z;
     }
 }
 
