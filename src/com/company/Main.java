30dc2af66f2e9bc6ecb5dca78b5e302b711967e1
Igor <gogy1507@gmail.com>
2021-07-29 16:37:04 +0400

    add comment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,22 +17,24 @@ public class Main {
         int triangle = in.nextInt();
         System.out.printf("Радиус: %s  высота: %s сегментов: %s  \n", radius, height, triangle);
         in.close();
-        outputPoints(inputX(triangle, radius), inputY(triangle, radius), inputZ(triangle, height));
-    }
-
+        System.out.println(3 > 1 ? "больше" : "меньше");
+        outputPoints(inputX(triangle, radius), inputY(triangle, radius), inputZ(triangle, height)); }
     private static void outputPoints(double[] inputX, double[] inputY, int[] inputZ) {
-        System.out.println("Координаты точек:");
-        for (int i = 0; i < inputX.length; i++) {
-System.out.println(String.format( "P%d: (" + "x=%.2f;  " + "y=%.2f;  " + "z=%d) ", i, inputX[i], inputY[i], inputZ[i]));
         System.out.println("Координаты точек треугольников:");
-System.out.println(String.format(
-"Треугольник%d: " +
-        "P(i)(" + "x=%.2f;  " + "y=%.2f;  " + "z=%d) + " +
-        "P(i+1)(" + "x=%.2f;  " + "y=%.2f;  " + "z=%d) " +
-        "A" + " x=0;  y=0;" + "z=%d",
-        i, inputX[i], inputY[i], inputZ[i],
-        inputX[i], inputY[i], inputZ[i],
-        inputZ[i]));}
+        // System.out.println("Координаты точек:");
+        for (int i = 0; i < inputX.length; i++) {
+         //   System.out.println(String.format("P%d: (" + "x=%.2f;  " + "y=%.2f;  " + "z=%d) ", i, inputX[i], inputY[i], inputZ[i]));
+
+            System.out.println(String.format(
+                    "Треугольник%d:\n " +
+                            "P(i)(" + "x=%.2f;  " + "y=%.2f;  " + "z=%d) + " +
+                            "P(i+1)(" + "x=%.2f;  " + "y=%.2f;  " + "z=%d) " +
+                            "A" + "x=0;  y=0;" + "z=%d",
+                    i,                                             // номер треугольника
+                    inputX[i], inputY[i], 0,               //координаты x; y; z; точки P(I)  inputZ[i]
+                    inputX[i], inputY[i], 0,               //координаты x; y; z; точки P(I+1)
+                    inputZ[i]));                                   //координаты x; y; z; точки A
+        }
     }
 
     public static double[] inputX(int triangle, int radius) {
@@ -42,6 +44,7 @@ System.out.println(String.format(
         }
         return arrayX;
     }
+
     public static double[] inputY(int triangle, int radius) {
         double[] arrayY = new double[triangle];
         for (int j = 0; j < triangle; j++) {
@@ -49,6 +52,7 @@ System.out.println(String.format(
         }
         return arrayY;
     }
+
     public static int[] inputZ(int triangle, int height) {
         int[] arrayZ = new int[triangle];
         for (int k = 0; k < triangle; k++) {
@@ -56,6 +60,7 @@ System.out.println(String.format(
         }
         return arrayZ;
     }
+
     public static double countX(int triangle, int radius, int i) {
         double x = 0;
         x = radius * Math.cos(6.28318531 * i / triangle);
